bde87f772bdb232339092a7d930651adedda6651
Speedster3030 <vonneumann87@gmail.com>
2026-02-25 16:05:20 +0530

    move

---
+++ ball.js
@@ -0,0 +1,34 @@
+
+
+let intervalId=null;
+let dir=1;
+
+function start()
+{
+    if(intervalId) return;
+    intervalId= setInterval(move,50);
+}
+
+function stop()
+{
+    clearInterval(intervalId);
+    intervalId=null;
+}
+
+function move()
+{
+    const ball= document.getElementById('ball');
+    let left= parseInt(ball.style.left)||0;
+    let newLeft= left+(5*dir);
+
+    if(newLeft>700)
+    {
+        dir=-1;
+    }
+    else if(newLeft<0)
+    {
+        dir=1;
+    }
+
+    ball.style.left= newLeft+'px';
+}
